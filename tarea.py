af48f840d6cb8ccb41bbb67b5738f3c45dd53792
J0harAR <62481969+J0harAR@users.noreply.github.com>
2020-03-21 13:08:51 -0600

    Add files via upload

---
+++ tarea.py
@@ -0,0 +1,17 @@
+print("#1")
+for i in  range (1,9):
+  print("*"*i)
+
+print("#2")
+for x in range (8,0,-1):
+    print("*"*x)
+
+print("#3")
+for i in  range (1,10):
+     print("*"*i)
+for x in range (10,0,-1):
+     print("*"*x)
+
+print("#4")
+for y in range (1,9):
+    print("*"*8)
